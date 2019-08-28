2bfcc62c4fb8b80b41a278b91c56c91b7a15c5c5
Veronica Lopez <nicaa0695@gmail.com>
2019-08-28 18:29:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  confugure do 
+    enable :sessions 
+    set :session_secret, "monty"
+  end 
+  
+  get '/' do 
+    erb :index 
+  end 
+  
 end
